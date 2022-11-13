93c901636cdbb9c44f8d352ec290c6e6c0a05377
= <=>
2022-11-13 18:53:51 +0100

    fix:nav-bar

--- src/components/Navigation/Nav.jsx
+++ src/components/Navigation/Nav.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import "./nav.scss"
+import logo from "../../assets/meta-logo.svg"
+import {GiHamburgerMenu} from "react-icons/gi"
+import { useState } from 'react'
 
 function Nav() {
+  const[toggle,setToggle]=useState(false)
   return (
-    <nav className='nav wrapper'>
-        <div className="logo">Meta</div>
-        <ul className="nav-links">
+    <nav>
+      <div className="nav wrapper">
+        <div className="logo">
+          <img src={logo} alt="meta-logo" />
+        </div>
+        <ul className={` ${toggle ? `toggle-active` : "nav-links"}`}>
           <li>Home</li>
           <li>Place to stay</li>
           <li>NFTs</li>
           <li>Community</li>
         </ul>
-        <button className='btn'>
-          Connect wallet
-        </button>
-        
+
+        <button className="btn">Connect wallet</button>
+
+        <div className="hambugger" onClick={() => setToggle(!toggle)}>
+          {!toggle && <GiHamburgerMenu />}
+          {toggle && <span>X</span>}
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Nav
